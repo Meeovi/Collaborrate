801d58ab6ee3a0ad124432ff85fcaebdb1b67492
Sebastian hilton <bastianh86@outlook.com>
2022-10-25 20:47:43 -0700

    fixed graphql issues with wrong query and mutations name inside of pages, removed more tables from database and their matching cms pages/files

--- components/newcustomers.vue
+++ components/newcustomers.vue
@@ -1,44 +1,45 @@
 <template>
-    <div>
-        <div class="table table-responsive table-bordered latestOrders col">
-                    <table class="table" >
-                        <thead>
-                            <tr>
-                                <th>Customer ID</th>
-                                <th>Customer First Name</th>
-                                <th>Customer Last Name</th>
-                                <th>Customer Since</th>
-                                <th>Email</th>
-                            </tr>
-                        </thead>
-                        <tbody v-for="customers in findFirstCustomers" :key="customers.id">
-                            <tr>
-                                <th scope="row">{{customers.id}}</th>
-                                <td>{{customers.first_name}}</td>
-                                <td>{{customers.last_name}}</td>
-                                <td>{{customers.customer_since}}</td>
-                                <td>{{customers.email}}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+  <div>
+    <div class="table table-responsive table-bordered latestOrders col">
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Customer ID</th>
+            <th>Customer First Name</th>
+            <th>Customer Last Name</th>
+            <th>Customer Since</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody v-for="customers in findFirstCustomers" :key="customers.id">
+          <tr>
+            <th scope="row">{{customers.id}}</th>
+            <td>{{customers.first_name}}</td>
+            <td>{{customers.last_name}}</td>
+            <td>{{customers.customer_since}}</td>
+            <td>{{customers.email}}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+  </div>
 </template>
 
 <script>
-import findFirstCustomers from '~/graphql/generated/queries/findManyCustomers'
+  import findFirstCustomers from '~/graphql/generated/queries/findFirstCustomers'
 
-export default {
-  data() {
-    return {
-      findFirstCustomers: [],
-    }
-  },
-  apollo: {
-    findFirstCustomers: {
-      prefetch: true,
-      query: findFirstCustomers
+  export default {
+    data() {
+      return {
+        findFirstCustomers: [],
+      }
     },
-  },
-}
+    apollo: {
+      findFirstCustomers: {
+        prefetch: true,
+        query: findFirstCustomers
+      },
+    },
+  }
+
 </script>
